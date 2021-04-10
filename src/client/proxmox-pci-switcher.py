70689a9ad087cf1a8b4b795c9055de83ebff6b6a
Rosiney Gomes Pereira <rosiney.gp@gmail.com>
2021-04-09 22:27:14 -0300

    feat: proxmox client switcher

---
+++ src/client/proxmox-pci-switcher.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+from proxmoxer import ProxmoxAPI
+import yaml
+import argh
+import os
+
+
+@argh.arg('name', help='machine name')
+@argh.arg('-c', '--config', help='yaml file path, otherwise .config.yaml')
+def proxmox_pci_switcher(name, config='~/.config/proxmox-pci-switcher/config.yaml'):
+  """Switcher virtual machine to use one pci resource like GPU"""
+
+  with open(os.path.expanduser(config)) as file:
+    pconfig = yaml.load(file, Loader=yaml.FullLoader)
+
+  proxmox = ProxmoxAPI(pconfig['proxmox']['host'],
+                       user=pconfig['proxmox']['user'],
+                       password=pconfig['proxmox']['password'],
+                       verify_ssl=pconfig['proxmox']['verify_ssl'])
+
+  # use first node
+  node = proxmox.nodes.get()[0]
+
+  target = False
+  for t in pconfig['targets']:
+    if name == t['name']:
+      target = t
+
+  if target:
+    if proxmox.nodes(node['node']).qemu(target['vmid']).status('current').get()['status'] == "stopped":
+      print(f"power on vm '{name}', see you later!")
+      proxmox.nodes(node['node']).qemu(target['vmid']).status('start').post()
+  else:
+    print(f"vm '{name}', not found in '{config}' file.")
+
+
+if __name__ == '__main__':
+  argh.dispatch_command(proxmox_pci_switcher)
