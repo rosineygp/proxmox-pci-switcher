6a8f8b59e6ebb2d1199f146de5e655d2c37567fc
Rosiney Gomes Pereira <rosiney.gp@gmail.com>
2021-04-14 05:53:44 -0300

    chore: autopep8 format

--- src/client/proxmox-pci-switcher.py
+++ src/client/proxmox-pci-switcher.py
@@ -9,34 +9,35 @@ import os
 @argh.arg('name', help='machine name')
 @argh.arg('-c', '--config', help='yaml file path, otherwise .config.yaml')
 def proxmox_pci_switcher(name, config='~/.config/proxmox-pci-switcher/config.yaml'):
-  """Switcher virtual machine to use one pci resource like GPU"""
+    """Switcher virtual machine to use one pci resource like GPU"""
 
-  with open(os.path.expanduser(config)) as file:
-    pconfig = yaml.load(file, Loader=yaml.FullLoader)
+    with open(os.path.expanduser(config)) as file:
+        pconfig = yaml.load(file, Loader=yaml.FullLoader)
 
-  proxmox = ProxmoxAPI(pconfig['proxmox']['host'],
-                       user=pconfig['proxmox']['user'],
-                       password=pconfig['proxmox']['password'],
-                       verify_ssl=pconfig['proxmox']['verify_ssl'])
+    proxmox = ProxmoxAPI(pconfig['proxmox']['host'],
+                         user=pconfig['proxmox']['user'],
+                         password=pconfig['proxmox']['password'],
+                         verify_ssl=pconfig['proxmox']['verify_ssl'])
 
-  # use first node
-  node = proxmox.nodes.get()[0]
+    # use first node
+    node = proxmox.nodes.get()[0]
 
-  target = False
-  for t in pconfig['targets']:
-    if name == t['name']:
-      target = t
-      break
+    target = False
+    for t in pconfig['targets']:
+        if name == t['name']:
+            target = t
+            break
 
-  if target:
-    if proxmox.nodes(node['node']).qemu(target['vmid']).status('current').get()['status'] == "stopped":
-      print(f"power on vm '{name}', see you later!")
-      proxmox.nodes(node['node']).qemu(target['vmid']).status('start').post()
+    if target:
+        if proxmox.nodes(node['node']).qemu(target['vmid']).status('current').get()['status'] == "stopped":
+            print(f"power on vm '{name}', see you later!")
+            proxmox.nodes(node['node']).qemu(
+                target['vmid']).status('start').post()
+        else:
+            print(f"target vm '{name}' is running.")
     else:
-      print(f"target vm '{name}' is running.")
-  else:
-    print(f"vm '{name}', not found in '{config}' file.")
+        print(f"vm '{name}', not found in '{config}' file.")
 
 
 if __name__ == '__main__':
-  argh.dispatch_command(proxmox_pci_switcher)
+    argh.dispatch_command(proxmox_pci_switcher)
